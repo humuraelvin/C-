201314afd684d274af0fb9bb280a966e5ca601c6
humuraelvin <humura53@gmail.com>
2024-10-22 15:32:44 +0200

    Uploded files

---
+++ Starting/shifting.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 181;
+
+    int k = a >> 1;
+    int t = a << 2;
+    int p = a >> 2;
+
+    // cout<<k<<endl;
+    // cout << t << endl;
+    // cout << p << endl;
+
+    // cout<<oct<<a<<endl;
+    // cout<<hex<<a<<endl;
+
+    int c = 100;
+
+    int d = c>>3;
+    int e = c<<3;
+
+    cout<<d<<endl;
+    cout<<e<<endl;
+
+    return 0;
+}
+

bbaa18f800a9c94368f0614943cc1619d05f96d8
humuraelvin <humura53@gmail.com>
2024-10-27 17:30:17 +0200

    changes made

--- WordGuess/WordGuessGame.cpp
+++ WordGuess/WordGuessGame.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
+
+void displayWord(const string& word, const unordered_set<char>& guessedLetters){
+    for (char c : word) 
+    {
+        if (guessedLetters.count(c))
+        {
+            cout << c;
+        }else{
+            cout << '_';
+        }
+        
+    }
+    
+}
 
 
 int main(){
 
-    
+
 
     return 0;
 }

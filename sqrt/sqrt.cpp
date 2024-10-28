79f617fb9d3907fd2003a56eeecae81dbca7d0d7
humuraelvin <humura53@gmail.com>
2024-10-28 15:08:54 +0200

    changes made

---
+++ sqrt/sqrt.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+
+
+    int a = 3;
+    int b = 5;
+
+    double hypotenuse = sqrt(pow(a, 2) + pow(b, 2));
+
+    cout << "The hypotenus is:" << hypotenuse <<endl;
+
+        return 0;
+}

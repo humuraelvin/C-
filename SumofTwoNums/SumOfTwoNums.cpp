6d48985d0387b71688d003309389213c46a6e0a1
humuraelvin <humura53@gmail.com>
2024-10-28 14:36:17 +0200

    changes made

--- SumofTwoNums/SumOfTwoNums.cpp
+++ SumofTwoNums/SumOfTwoNums.cpp
@@ -2,12 +2,27 @@
 using namespace std;
 
 int addTwoNums(int a, int b){
+    return a+b;
+}
+
+int addTwoNums(int a, int b, int c){
+    return a+b+c;
+}
+
+int addTwoNums(double a, double b){
     return a + b;
+}
+
+int addTwoNums(double a, double b, double c){
+    return a+b+c;
 }
 
 int main(){
 
     cout << "Result: "<<addTwoNums(10, 20) <<endl;
+    cout << "Result: " << addTwoNums(10, 20, 30) << endl;
+    cout << "Result: " << addTwoNums(10.5, 20.3) << endl;
+    cout << "Result: " << addTwoNums(10.5, 20.3, 45.23) << endl;
 
     return 0;
 }

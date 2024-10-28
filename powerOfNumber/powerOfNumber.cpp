1b105a5c3e509026e1da7542a3256a172f8984a0
humuraelvin <humura53@gmail.com>
2024-10-28 14:25:50 +0200

    changes made

---
+++ powerOfNumber/powerOfNumber.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void calculatePower(int number, int power){
+
+    for (int i = 1; i < power; i++)
+    {
+        number *= number;
+    }
+    
+    cout << "Result: " << number<< endl;
+
+}
+
+int main(){
+
+    calculatePower(7, 2);
+
+}

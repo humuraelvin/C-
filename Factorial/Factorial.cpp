fe75f753facdc2b84d3c05ceb6d3c318ff4b018a
humuraelvin <humura53@gmail.com>
2024-10-29 12:06:20 +0200

    changes made

--- Factorial/Factorial.cpp
+++ Factorial/Factorial.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 int  printFactorial(int n){
 
-    return n * printFactorial(n - 1);
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }else
+    {
+        return n * printFactorial(n - 1);
+    }
+    
+    
+
 
 }
 

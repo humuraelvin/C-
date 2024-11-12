89e915701850035e46c0a73f7cf6b80020fb0da6
humuraelvin <humura53@gmail.com>
2024-11-12 14:00:59 +0200

    changes made

---
+++ Arrays/arr2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int numbers[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout <<numbers[i]<<endl;
+    }
+    
+
+    return 0;
+}

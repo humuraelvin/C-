79df5433c97d72f8b37dbb3de4bb77f2ddeb2e72
humuraelvin <humura53@gmail.com>
2024-11-12 14:52:12 +0200

    changes made

---
+++ Arrays/array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+
+int main(){
+
+    int arr[] = {11, 12, 13, 14, 15};
+
+    cout<<"The length is :" << end(arr) - begin(arr);
+
+    return 0;
+
+}

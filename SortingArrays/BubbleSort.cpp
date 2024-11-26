ab50f1f1ccd28395b96ef220e6e60b8fa99d3810
humuraelvin <humura53@gmail.com>
2024-11-26 14:20:25 +0200

    changes added

--- SortingArrays/BubbleSort.cpp
+++ SortingArrays/BubbleSort.cpp
@@ -7,6 +7,9 @@ void bubbleSort(int arr[], int n)
     {
         for (int j = 0; j < n - 1 - i; j++)
         {
+
+            cout<<"i="<<i<<" j = "<<j<<" i*j = "<<i*j<<endl;
+
             if (arr[j] > arr[j + 1])
             {
                 int temp = arr[j];
@@ -28,8 +31,15 @@ void printArray(int arr[], int n)
 
 int main()
 {
-    int arr[10] = {9, 7, 2, 5, 4, 10, 3, 8, 1, 6};
+
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // {9, 7, 2, 5, 4, 10, 3, 8, 1, 6}
+
     int n = sizeof(arr) / sizeof(arr[0]);
+
+
     bubbleSort(arr, n);
     printArray(arr, n);
+
+    return 0;
 }

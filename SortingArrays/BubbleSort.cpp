def882edfdc12ffa5002d6d45e3faffe1cf7e306
humuraelvin <humura53@gmail.com>
2024-11-26 13:52:26 +0200

    changes added

--- SortingArrays/BubbleSort.cpp
+++ SortingArrays/BubbleSort.cpp
@@ -1,44 +1,35 @@
 #include <iostream>
 using namespace std;
 
-
-
-void bubbleSort(int arr[], int n){
-
-for (int i = 0; i < n-1; i++)
+void bubbleSort(int arr[], int n)
 {
-    for (int j = 0; i < n-1; j++)
+    for (int i = 0; i < n - 1; i++)
     {
-        if (arr[j] >arr[j+1])
+        for (int j = 0; j < n - 1 - i; j++)
         {
-            int temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
-        
     }
-    
-}
-
 }
 
 void printArray(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << endl;
-        cout << endl;
+        cout << arr[i] << " "; 
     }
+    cout << endl; 
 }
 
-int main(){
-
+int main()
+{
     int arr[10] = {9, 7, 2, 5, 4, 10, 3, 8, 1, 6};
-
-    int n = sizeof(arr)/sizeof(arr[0]);
-
+    int n = sizeof(arr) / sizeof(arr[0]);
     bubbleSort(arr, n);
-
     printArray(arr, n);
-
 }
